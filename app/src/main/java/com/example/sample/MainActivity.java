d35ae91411c5999cc27ae45200f18e7e4868bb33
Gene Romuga <gcpromuga@letran-calamba.edu.ph>
2022-06-13 16:41:50 +0800

    Added button

--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button btnSample;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
